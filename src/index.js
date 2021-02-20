379e8d54aff1a8c581ffbb477fed797aa710abf5
Denis <wolf-den@tut.by>
2021-02-20 13:45:27 +0300

    feat: function ckeck for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str, bracketsConfig)
+  let arr = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (arr !== []) {
+      for (let y = 0; y < bracketsConfig.length; y++) {
+        for (let x = 0; x < 2; x++) {
+          if (arr[i] === bracketsConfig[y][x]) {
+            if (arr[i + 1] === bracketsConfig[y][x + 1]) {
+              if (bracketsConfig[y][x + 1] === undefined) break;
+              arr.splice(i, 2);
+              i = -1;
+
+            }
+          }
+        }
+      }
+    }
+  }
+  console.log(arr)
+  if (arr.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
