5b7023236cd562b552b54af61f4203898269f3fb
Denis <wolf-den@tut.by>
2021-02-20 13:56:42 +0300

    fix: function check

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  console.log(str, bracketsConfig)
   let arr = str.split('');
   for (let i = 0; i < str.length; i++) {
-    if (arr !== []) {
-      for (let y = 0; y < bracketsConfig.length; y++) {
-        for (let x = 0; x < 2; x++) {
-          if (arr[i] === bracketsConfig[y][x]) {
-            if (arr[i + 1] === bracketsConfig[y][x + 1]) {
-              if (bracketsConfig[y][x + 1] === undefined) break;
-              arr.splice(i, 2);
-              i = -1;
-
-            }
+    for (let y = 0; y < bracketsConfig.length; y++) {
+      for (let x = 0; x < 2; x++) {
+        if (bracketsConfig[y][x + 1] === undefined) break;
+        if (arr[i] === bracketsConfig[y][x]) {
+          if (arr[i + 1] === bracketsConfig[y][x + 1]) {
+            arr.splice(i, 2);
+            i = -1;
           }
         }
       }
     }
   }
-  console.log(arr)
-  if (arr.length === 0) {
-    return true
-  } else {
-    return false
-  }
+  return arr.length === 0
 }
